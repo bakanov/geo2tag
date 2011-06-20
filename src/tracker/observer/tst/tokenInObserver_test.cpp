34eb712eb065f7ac58ebe3534da9d624fcd136e4
ReginaDorokhova <stav-gina@mail.ru>
2011-06-20 14:13:32 +0400

    update test for token in observer

--- src/tracker/observer/tst/tokenInObserver_test.cpp
+++ src/tracker/observer/tst/tokenInObserver_test.cpp
@@ -19,7 +19,6 @@ tokenInObserver_test::tokenInObserver_test(QObject *parent):QObject(parent), m_d
     QStringList list = reply.split('"');
     QString token = list.at(3);
     m_token = token;
-    qDebug()<<"TOKEN="<<token;
 
 }
 
@@ -28,19 +27,28 @@ tokenInObserver_test::~tokenInObserver_test()
 
 }
 
-void tokenInObserver_test::testRequest()
+void tokenInObserver_test::testFeedRequestWithCorrectToken()
 {
     QByteArray rssFeedRequest;
     rssFeedRequest.append(requestBegin+m_token+requestEnd);
     QString reply = QString(m_db.process("rss", rssFeedRequest).data());
-    //m_db.forceUpdate();
-    int posBegin = reply.lastIndexOf("status");
-    posBegin = reply.lastIndexOf(":",posBegin);
-    int posEnd = reply.lastIndexOf(",",posBegin);
-    reply.remove(0,posBegin+2);
-    reply.remove(posEnd-1, reply.length()-posEnd);
-    qDebug()<<reply;
-    QCOMPARE(0,reply.compare("Ok"));
+    int posBegin = reply.indexOf("\"status\" :");
+    reply.remove(0,posBegin);
+    int posEnd = reply.indexOf(" }",0);
+    reply.remove(posEnd, reply.length());
+    QCOMPARE(0,reply.compare(correctReplyPart));
 }
 
+
+void tokenInObserver_test::testFeedRequestWithWrongToken()
+{
+    QByteArray rssFeedRequest;
+    rssFeedRequest.append(requestBegin+m_token+wrongToken+requestEnd);
+    QString reply = QString(m_db.process("rss", rssFeedRequest).data());
+    int posBegin = reply.indexOf("\"status\" :");
+    reply.remove(0,posBegin);
+    int posEnd = reply.indexOf(" }",0);
+    reply.remove(posEnd, reply.length());
+    QCOMPARE(0,reply.compare(wrongReplyPart));
+}
 }
--- src/tracker/observer/tst/tokenInObserver_test.h
+++ src/tracker/observer/tst/tokenInObserver_test.h
@@ -1,17 +1,18 @@
 #include <QtCore/QString>
 #include <QtTest/QtTest>
 #include <QtSql>
-#include </home/regina/QtSDK/Desktop/Qt/473/gcc/include/QtSql/qsql_psql.h>
 #include "../../../http_requests/inc/LoginQuery.h"
 #include "../../../http_requests/inc/RSSFeedQuery.h"
 #include "../../../service/inc/DbSession.h"
 
 namespace Test
 {
-const QString loginRequest = "{\"login\":\"tracker\",\"password\":\"test\"}";
+const QString loginRequest = "{\"login\":\"Kirill\",\"password\":\"test\"}";
 const QString requestBegin = "{\"auth_token\":\"";
 const QString requestEnd = "\", \"latitude\":30.0, \"longitude\":60.0, \"radius\":30.0}";
-
+const QString correctReplyPart = "\"status\" : \"Ok\", \"status_description\" : \"feed has been generated\"";
+const QString wrongReplyPart = "\"status\" : \"Error\", \"status_description\" : \"Wrong authentification key\"";
+const QString wrongToken = "wrongToken";
 class tokenInObserver_test : public QObject
 {
     Q_OBJECT
@@ -23,7 +24,8 @@ public:
 
     ~tokenInObserver_test();
 private slots:
-    void testRequest();
+    void testFeedRequestWithCorrectToken();
+    void testFeedRequestWithWrongToken();
 
 
 };
